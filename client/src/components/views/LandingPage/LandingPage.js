2210ee6e9ecbb41939835966369daf747cf0b6d7
AlwaysLee-Creater <54054460+AlwaysLee-Creater@users.noreply.github.com>
2021-05-06 13:31:40 +0900

    LandingPage의 grid cards component 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -4,6 +4,8 @@ import {withRouter} from 'react-router-dom'
 import {FaCode} from "react-icons/fa"
 import {API_KEY, API_URL, IMAGE_BASE_URL} from '../../Config';
 import MainImage from './Sections/MainImage'
+import GridCards from '../commons/GridCards'
+import {Row} from 'antd'
 
 function LandingPage() {
     const [Movies,setMovies]=useState([])
@@ -36,7 +38,18 @@ function LandingPage() {
                 <h2>Movies by latest</h2>
                 <hr/>
 
-                {/*Movie Grid Cards*/}        
+                {/*Movie Grid Cards*/}    
+                <Row gutter={[16,16]}>
+                    {Movies && Movies.map((movie,index)=>(
+                        <React.Fragment key={index}>
+                            <GridCards
+                                image={movie.poster_path?`${IMAGE_BASE_URL}w500${movie.poster_path}`:null}
+                                movieId={movie.id}
+                                movieName={movie.original_title}
+                            />
+                        </React.Fragment>
+                    ))}
+                </Row>              
             </div>
 
             <div style={{display:'flex',justifyContent:'center'}}>
