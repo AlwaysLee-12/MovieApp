4a491ccc352d6aa90f9b783d2898901735cbcaf4
AlwaysLee-Creater <54054460+AlwaysLee-Creater@users.noreply.github.com>
2021-05-09 17:00:06 +0900

    좋아요 클릭 여부 표시하기(버튼 클릭 전후 변화)

--- client/src/components/views/MovieDetail/Sections/Favorite.js
+++ client/src/components/views/MovieDetail/Sections/Favorite.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import React,{useEffect} from 'react'
+import React,{useEffect,useState} from 'react'
 
 function Favorite(props) {
     const movieId=props.movieId
@@ -7,6 +7,9 @@ function Favorite(props) {
     const movieTitle=props.movieInfo.title
     const moviePost=props.movieInfo.backdrop_path
     const movieRunTime=props.movieInfo.runtime
+    
+    const [FavoriteNumber,setFavoriteNumber]=useState(0)
+    const [Favorited,setFavorited]=useState(false)
 
     useEffect(() => {
         let variables={
@@ -17,17 +20,27 @@ function Favorite(props) {
         axios.post('/api/favorite/favoriteNumber',variables)
             .then(response=>{
                 console.log(response.data);
+                setFavoriteNumber(response.data.favoriteNumber)
                 if(response.data.success){
 
                 }else{
                     alert('숫자 정보를 가져오는데 실패.')
                 }
             })
+
+        axios.post('/api/favorite/favorited',variables)
+            .then(response=>{
+                if(response.data.success){
+                    setFavorited(response.data.favorited)
+                }else{
+                    alert('정보를 가져오는데 실패.')
+                }
+            })
     }, [])
 
     return (
         <div>
-            <button>Favorite</button>
+            <button>{Favorited? "Not Favorite":"Add to Favorite"}{FavoriteNumber}</button>
         </div>
     )
 }
--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -4,7 +4,6 @@ const {Favorite}=require('../models/Favorite');
 
 router.post('/favoriteNumber',(req,res)=>{
     req.body.movieId
-
     //mongoDB에서 favorite 숫자 가져오기
     Favorite.find({"movieId":req.body.movieId})
     .exec((err,info)=>{
@@ -13,5 +12,24 @@ router.post('/favoriteNumber',(req,res)=>{
         res.status(200).json({success:true, favoriteNumber:info.length})
     }) 
 })
+
+router.post('/favorited',(req,res)=>{
+    req.body.movieId
+    //해당 영화를 좋아요 리스트에 넣었는지에 대한 정보를 DB에서 가져옴
+
+    //DB에서 favorite 숫자 가져옴
+    Favorite.find({"movieId":req.body.movieId,"userFrom":req,body,userFrom})
+        .exec((err,info)=>{
+            if(err) return res.status(400).send(err)
+            //프론트에 다시 숫자 정보 보내기
+
+            let result=false;
+            if(info.length!==0){
+                result=true
+            }
+            res.status(200).json({success:true, favorited:result})
+    }) 
+})
+
 
 module.exports=router;
